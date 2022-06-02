90a085b52058e08a1db2b5d187767fefc834272b
Stéphane Guerreau <contact@arcadia-labs.com>
2022-06-02 15:53:35 +0200

    Update for Php7

--- include/mysql_connect.php
+++ include/mysql_connect.php
@@ -4,7 +4,7 @@
 	$db_pass = "DB_PASSWORD";
 	$db_name = "DB_NAME";
 
-	($GLOBALS["___mysqli_ston"] = mysqli_connect($db_host,  $db_username,  $db_pass)) or die ("Could not connect connect to MySQL Server");
+	($GLOBALS["___mysqli_ston"] = mysqli_connect($db_host,  $db_username,  $db_pass, $db_name, 3307)) or die ("Could not connect connect to MySQL Server");
 	((bool)mysqli_query($GLOBALS["___mysqli_ston"], "USE $db_name")) or die ("No database");
 	mysqli_set_charset($GLOBALS["___mysqli_ston"], 'utf8');
 ?>
